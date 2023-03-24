ac3299b5ed269cc865cd78ddb7d0c253ce4209e2
TheFlagen430297 <support@theflagen430297.com>
2023-03-24 12:27:32 -0500

    Commit 4: Making way with Mobile!
    Started work on mobile support and adding features

--- root/beta-source-code.js
+++ root/beta-source-code.js
@@ -1,14 +1,47 @@
-setTimeout(() => {
-    $(`#background-container #BGG1`).addClass(`fade-in-longer red-background`);
-    $(`#background-container #BGG2`).addClass(`fade-in-longer black-background`);
+$(`#navigation-container #content #trademark`).click(() => {
+    let SearchString = /(http(?:s|)):(?:\/*)(.*)\/(?:[a-zA-Z0-9.]*)/
+    let Part = SearchString.exec(window.location.href)
+    window.location.href = `${Part[1]}://${Part[2]}`;
+})
+
+$(`#navigation-container #content #trademark`).hover(() => {
+    $(`#navigation-container #content #trademark #text #span`).addClass(`hover`)
+}, () => {
+    $(`#navigation-container #content #trademark #text #span`).removeClass(`hover`)
+})
+
+if ((Math.random() * (100 - 0) + 0) > 55) $(`#navigation-container #content #trademark #text #span`).addClass(`VALORANTFontBlink`);
+
+$(`#navigation-container #mobile-container #toggle`).click(() => {
+    if ($(`#content-container #mobile-container`).css(`display`) == `none`) {
+        $(`#content-container #mobile-container`).css(`display`, `block`)
+        $(`#content-container #content`).css(`display`, `none`)
+    } else {
+        $(`#content-container #mobile-container`).css(`display`, `none`)
+        $(`#content-container #content`).css(`display`, `block`)
+    }
+})
+
+if (screen.width < 700) {
+    $(`#background-container #BGG1`).addClass(`red-background`);
+    $(`#background-container #BGG2`).addClass(`black-background`);
+    $(`#content-container #content`).addClass(`tracking-in-expand`);
+} else {
+    $(`#background-container #picture`).addClass(`scale-up-center`)
+
     setTimeout(() => {
-        $(`#content-container`).addClass(`fade-in`);
-        $(`#content-container`).css(`display`, `block`);
-        $(`#content-container #content`).addClass(`slide-in-left`);
+        $(`#background-container #picture`).removeClass(`scale-up-center`)
+        $(`#background-container #BGG1`).addClass(`fade-in-longer red-background`);
+        $(`#background-container #BGG2`).addClass(`fade-in-longer black-background`);
         setTimeout(() => {
-            $(`#navigation-container`).addClass(`slide-in-top`);
-            $(`#navigation-container #content`).addClass(`slide-in-top-longer`);
-            $(`#navigation-container`).css(`display`, `block`);
-        }, 700);
-    }, 1000);
-}, 1500);
+            $(`#content-container`).addClass(`fade-in`);
+            $(`#content-container`).css(`display`, `block`);
+            $(`#content-container #content`).addClass(`slide-in-left`);
+            setTimeout(() => {
+                $(`#navigation-container`).addClass(`slide-in-top`);
+                $(`#navigation-container #content`).addClass(`slide-in-top-longer`);
+                $(`#navigation-container`).css(`display`, `block`);
+            }, 700);
+        }, 1E3);
+    }, 1.5E3);
+}
